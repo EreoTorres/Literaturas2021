1a3d18afcdc48672071be52bcbd14b2c3b48a81e
Adriana Hernández Marrujo <37121433+Adrianahdez19@users.noreply.github.com>
2022-04-26 15:21:31 -0600

    Modulo de Gaceta

---
+++ src/app/academica/academica-menu.ts
@@ -0,0 +1,26 @@
+export const MENU_ITEMS = [
+    {
+      title: 'Literaturas',
+      icon: 'sticky_note_2_outline',
+      link: '/academica/literaturas',
+      home: true,
+    },
+    {
+        title: 'Videos',
+        icon: 'videocam_outline',
+        link: '/academica/videos',
+        home: true,
+    },
+    {
+      title: 'Materias',
+      icon: 'videocam_outline',
+      link: '/academica/materias',
+      home: true,
+    },
+    {
+      title: 'Gaceta',
+      icon: 'videocam_outline',
+      link: '/academica/gaceta',
+      home: true,
+    }
+];
