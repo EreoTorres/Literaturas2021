910b819c7f01c8dae7d5260acefd3b4d4a1491c7
Adriana Hernandez <adrianahdez019@gmail.com>
2022-06-21 13:06:52 -0600

    Actualizar ruta api desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: true,
   urlPruebasLocal: 'http://localhost:3000/',
   urlDesarrollo: 'http://literaturas-dev.us-east-2.elasticbeanstalk.com/',
-  urlProduccion: 'http://literaturas-env.eba-mp2evwet.us-east-2.elasticbeanstalk.com/'
+  urlProduccion: 'http://literaturas-dev.us-east-2.elasticbeanstalk.com/'
 };
 
 /*
